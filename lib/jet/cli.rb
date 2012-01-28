63471a67411d8ab030b295756511ab0f3c8b2d2b
Erwan Barrier <erwan.barrier@gmail.com>
2012-01-28 16:32:03 +0100

    add shortcut for starting server and generating a prototype

--- lib/jet/cli.rb
+++ lib/jet/cli.rb
@@ -4,10 +4,11 @@ module Jet
   class CLI < Thor
     include Thor::Actions
 
-    desc 'server', 'start the Jet server'
+    desc 'server', 'start the Jet server (short-cut alias: "s")'
     def server
       ::Rack::Server.start(:config => 'config.ru', :Port => 5000)
     end
+    map 's' => 'server'
 
     def self.source_root
       ::File.expand_path(::File.join(::File.dirname(__FILE__), '../../templates'))
@@ -22,12 +23,13 @@ module Jet
       directory 'application', target
     end
 
-    desc 'generate_prototype NAME', 'generates a new prototype'
+    desc 'generate_prototype NAME', 'generates a new prototype (short-cut alias: "gp")'
     def generate_prototype(name)
       # name = Utils.underscore(name)
       target = ::File.join(Dir.pwd, 'test/prototypes', name)
 
       directory 'prototype', target
     end
+    map 'gp' => 'generate_prototype'
   end
 end
